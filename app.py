65d59d8f2b0af9eb07ffe9fdc161cea8cbb0260d
mateusbt <mateus.trigueiro@academico.ifpb.edu.br>
2023-07-28 12:20:35 -0300

    blabla

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+
+
+dados=[]
+class Usuario:
+
+    def __init__(self,usuario,senha):
+        self.usuario=usuario
+        self.senha=senha        
+
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY']= 'XUXA'
 
 @app.route('/')
 def hello_world(): 
-    return render_template('home.html')
+    return render_template('home.html',log = session)
 
 @app.route('/login')
 def teste2(): 
@@ -20,11 +31,32 @@ def principal():
 def login():
     login = request.form['usuario']
     senha = request.form['senha']
-    if login == 'aluno' and senha == '1234':
-        return redirect(url_for('hello_world'))
-    else:        
-        return redirect(url_for('teste2'))
+    for dado in dados:
+        if dado.usuario==login:
+            if dado.senha==senha:
+                session['logado']=dado.usuario
+                return redirect('/')
+    return redirect('/login')
+    
 
+@app.route('/registro',methods=['POST'])
+def cadastro():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    confirmsenha=request.form['senha2']
+
+    if senha==confirmsenha:
+        dados.append(Usuario(usuario,senha))
+        return redirect ('/login')
+    return redirect ('/registro')
+
+
+@app.route('/sair')
+def sair():
+    del session['logado']
+    return redirect ('/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
